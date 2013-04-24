e6fc03d21b1cfdb7d7f4e2ffb5487028b7f23103
Ryan <ryanclancy000@gmail.com>
2013-04-24 18:54:26 -0300

    Don't make a new instance in of my list in the initConfig method (or try and clear a non defined list)

--- src/main/java/com/ryanclancy000/plugman/PlugMan.java
+++ src/main/java/com/ryanclancy000/plugman/PlugMan.java
@@ -3,6 +3,7 @@ package com.ryanclancy000.plugman;
 import com.ryanclancy000.plugman.utilities.MetricsLite;
 import com.ryanclancy000.plugman.utilities.Utilities;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 
@@ -10,10 +11,9 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class PlugMan extends JavaPlugin {
 
-    private List<String> skipPlugins;
-
     private final Utilities utils = new Utilities(this);
-
+    private final List<String> skipPlugins = new ArrayList<String>();
+    
     @Override
     public void onDisable() {
         skipPlugins.clear();
@@ -28,29 +28,31 @@ public class PlugMan extends JavaPlugin {
 
     private void initConfig() {
         try {
-            this.getConfig().options().copyDefaults(true);
-            skipPlugins = this.getConfig().getStringList("skip-on-reload");
-            this.saveConfig();
-        } catch (Exception e) {
-            this.getLogger().log(Level.SEVERE, "Failed to load config - ignoring skip-plugins feature!{0}", e);
+            getConfig().options().copyDefaults(true);
+            for (final String s : getConfig().getStringList("skip-on-reload")) {
+                skipPlugins.add(s);
+            }
+            saveConfig();
+        } catch (final Exception e) {
+            getLogger().log(Level.SEVERE, "Failed to load config - ignoring skip-plugins feature!{0}", e);
             skipPlugins.clear();
         }
     }
 
     private void initCommands() {
-        this.getCommand("plugman").setExecutor(new PlugManCommands(this));
+        getCommand("plugman").setExecutor(new PlugManCommands(this));
     }
 
     private void initMetrics() {
-        if (this.getConfig().getBoolean("use-metrics")) {
+        if (getConfig().getBoolean("use-metrics")) {
             try {
                 new MetricsLite(this).start();
-                this.getLogger().log(Level.INFO, "Metrics successfully started!");
-            } catch (Exception e) {
-                this.getLogger().log(Level.SEVERE, "Failed to start Metrics!{0}", e);
+                getLogger().log(Level.INFO, "Metrics successfully started!");
+            } catch (final Exception e) {
+                getLogger().log(Level.SEVERE, "Failed to start Metrics!{0}", e);
             }
         } else {
-            this.getLogger().log(Level.INFO, "Ignoring Metrics!");
+            getLogger().log(Level.INFO, "Ignoring Metrics!");
         }
     }
 
