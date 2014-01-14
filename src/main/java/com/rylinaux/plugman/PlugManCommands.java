04ae7937cd10389ae78c77f7ed3526b8f9a673d7
rylinaux <rylinaux@gmail.com>
2014-01-14 01:55:20 -0400

    Listen for the Disable and List commands

--- src/main/java/com/rylinaux/plugman/PlugManCommands.java
+++ src/main/java/com/rylinaux/plugman/PlugManCommands.java
@@ -1,6 +1,8 @@
 package com.rylinaux.plugman;
 
+import com.rylinaux.plugman.commands.DisableCommand;
 import com.rylinaux.plugman.commands.EnableCommand;
+import com.rylinaux.plugman.commands.ListCommand;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -11,8 +13,16 @@ public class PlugManCommands implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
+        if (args[0].equalsIgnoreCase("disable")) {
+            new DisableCommand(sender).execute(sender, command, label, args);
+        }
+
         if (args[0].equalsIgnoreCase("enable")) {
             new EnableCommand(sender).execute(sender, command, label, args);
+        }
+
+        if (args[0].equalsIgnoreCase("list")) {
+            new ListCommand(sender).execute(sender, command, label, args);
         }
 
         return true;
