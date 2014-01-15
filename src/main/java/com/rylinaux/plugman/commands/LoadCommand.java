98df087b55e506c64c4674cc7cf1869f7b9df635
rylinaux <rylinaux@gmail.com>
2014-01-15 14:56:38 -0400

    Implement load command.

--- src/main/java/com/rylinaux/plugman/commands/LoadCommand.java
+++ src/main/java/com/rylinaux/plugman/commands/LoadCommand.java
@@ -1,5 +1,51 @@
 package com.rylinaux.plugman.commands;
 
-public class LoadCommand {
+import com.rylinaux.plugman.PlugMan;
+import com.rylinaux.plugman.utilities.PluginUtils;
 
+import java.io.File;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.InvalidDescriptionException;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginDescriptionFile;
+
+public class LoadCommand extends AbstractCommand {
+
+    public static final String DESCRIPTION = "Load a plugin.";
+
+    public static final String PERMISSION = "plugman.load";
+
+    public static final String USAGE = "/plugman enable [plugin]";
+
+    public static final String[] SUB_PERMISSIONS = {""};
+
+    public LoadCommand(CommandSender sender) {
+        super(sender, DESCRIPTION, PERMISSION, SUB_PERMISSIONS, USAGE);
+    }
+
+    @Override
+    public void execute(CommandSender sender, Command command, String label, String[] args) {
+
+        if (!hasPermission()) {
+            sender.sendMessage(PlugMan.getInstance().getMessageManager().format("error.no-permission"));
+            return;
+        }
+
+        if (args.length < 2) {
+            sender.sendMessage(PlugMan.getInstance().getMessageManager().format("error.specify-plugin"));
+            return;
+        }
+
+        Plugin potential = PluginUtils.getPluginByName(args, 1);
+
+        if (potential != null) {
+            sender.sendMessage(PlugMan.getInstance().getMessageManager().format("load.already-loaded", potential.getName()));
+            return;
+        }
+
+        sender.sendMessage(PluginUtils.load(args));
+
+    }
 }
