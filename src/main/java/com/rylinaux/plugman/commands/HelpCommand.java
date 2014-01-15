b41e0f2f615ac93a1f2ea42d585df06de0bd6bcb
rylinaux <rylinaux@gmail.com>
2014-01-15 14:54:31 -0400

    Convert to new format() method.

--- src/main/java/com/rylinaux/plugman/commands/HelpCommand.java
+++ src/main/java/com/rylinaux/plugman/commands/HelpCommand.java
@@ -31,7 +31,7 @@ public class HelpCommand extends AbstractCommand {
         ConfigurationSection help = PlugMan.getInstance().getMessageManager().getMessaging().getConfig().getConfigurationSection("help");
 
         for (String s : help.getKeys(false)) {
-            sender.sendMessage(PlugMan.getInstance().getMessageManager().raw(help.getName() + "." + s));
+            sender.sendMessage(PlugMan.getInstance().getMessageManager().format(false, help.getName() + "." + s));
         }
 
     }
