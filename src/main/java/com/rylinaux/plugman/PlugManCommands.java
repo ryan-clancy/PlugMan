56d4bf9b7f5cb7634ee669d6a99b0f610fc65ec1
rylinaux <rylinaux@gmail.com>
2014-01-15 16:02:26 -0400

    List for new commands. Add return statements to avoid always showing help.

--- src/main/java/com/rylinaux/plugman/PlugManCommands.java
+++ src/main/java/com/rylinaux/plugman/PlugManCommands.java
@@ -6,7 +6,10 @@ import com.rylinaux.plugman.commands.HelpCommand;
 import com.rylinaux.plugman.commands.InfoCommand;
 import com.rylinaux.plugman.commands.ListCommand;
 import com.rylinaux.plugman.commands.LoadCommand;
+import com.rylinaux.plugman.commands.ReloadCommand;
 import com.rylinaux.plugman.commands.RestartCommand;
+import com.rylinaux.plugman.commands.UnloadCommand;
+import com.rylinaux.plugman.commands.UsageCommand;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -24,26 +27,47 @@ public class PlugManCommands implements CommandExecutor {
 
         if (args[0].equalsIgnoreCase("disable")) {
             new DisableCommand(sender).execute(sender, command, label, args);
+            return true;
         }
 
         if (args[0].equalsIgnoreCase("enable")) {
             new EnableCommand(sender).execute(sender, command, label, args);
+            return true;
         }
 
         if (args[0].equalsIgnoreCase("info")) {
             new InfoCommand(sender).execute(sender, command, label, args);
+            return true;
         }
 
         if (args[0].equalsIgnoreCase("list")) {
             new ListCommand(sender).execute(sender, command, label, args);
+            return true;
         }
 
         if (args[0].equalsIgnoreCase("load")) {
             new LoadCommand(sender).execute(sender, command, label, args);
+            return true;
         }
 
         if (args[0].equalsIgnoreCase("restart")) {
             new RestartCommand(sender).execute(sender, command, label, args);
+            return true;
+        }
+
+        if (args[0].equalsIgnoreCase("reload")) {
+            new ReloadCommand(sender).execute(sender, command, label, args);
+            return true;
+        }
+
+        if (args[0].equalsIgnoreCase("unload")) {
+            new UnloadCommand(sender).execute(sender, command, label, args);
+            return true;
+        }
+
+        if (args[0].equalsIgnoreCase("usage")) {
+            new UsageCommand(sender).execute(sender, command, label, args);
+            return true;
         }
 
         new HelpCommand(sender).execute(sender, command, label, args);
