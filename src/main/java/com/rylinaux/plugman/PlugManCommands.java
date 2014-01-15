a285c37cef535ab1dbb278c6aeea502c9e9813c5
rylinaux <rylinaux@gmail.com>
2014-01-15 14:56:16 -0400

    Listen for load command. Change where help is displayed.

--- src/main/java/com/rylinaux/plugman/PlugManCommands.java
+++ src/main/java/com/rylinaux/plugman/PlugManCommands.java
@@ -5,6 +5,7 @@ import com.rylinaux.plugman.commands.EnableCommand;
 import com.rylinaux.plugman.commands.HelpCommand;
 import com.rylinaux.plugman.commands.InfoCommand;
 import com.rylinaux.plugman.commands.ListCommand;
+import com.rylinaux.plugman.commands.LoadCommand;
 import com.rylinaux.plugman.commands.RestartCommand;
 
 import org.bukkit.command.Command;
@@ -16,16 +17,17 @@ public class PlugManCommands implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
+        if (args.length == 0 || args[0].equalsIgnoreCase("help")) {
+            new HelpCommand(sender).execute(sender, command, label, args);
+            return true;
+        }
+
         if (args[0].equalsIgnoreCase("disable")) {
             new DisableCommand(sender).execute(sender, command, label, args);
         }
 
         if (args[0].equalsIgnoreCase("enable")) {
             new EnableCommand(sender).execute(sender, command, label, args);
-        }
-
-        if (args[0].equalsIgnoreCase("help")) {
-            new HelpCommand(sender).execute(sender, command, label, args);
         }
 
         if (args[0].equalsIgnoreCase("info")) {
@@ -36,12 +38,16 @@ public class PlugManCommands implements CommandExecutor {
             new ListCommand(sender).execute(sender, command, label, args);
         }
 
+        if (args[0].equalsIgnoreCase("load")) {
+            new LoadCommand(sender).execute(sender, command, label, args);
+        }
+
         if (args[0].equalsIgnoreCase("restart")) {
             new RestartCommand(sender).execute(sender, command, label, args);
         }
 
         new HelpCommand(sender).execute(sender, command, label, args);
-        
+
         return true;
     }
 
