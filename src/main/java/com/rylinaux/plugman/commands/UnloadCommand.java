8c80b6386da03bf343e6e78f75f7f380b7c18d4e
rylinaux <rylinaux@gmail.com>
2014-01-15 16:01:48 -0400

    Implement reload, unload, and usage commands.

--- src/main/java/com/rylinaux/plugman/commands/UnloadCommand.java
+++ src/main/java/com/rylinaux/plugman/commands/UnloadCommand.java
@@ -1,5 +1,54 @@
 package com.rylinaux.plugman.commands;
 
-public class UnloadCommand {
+import com.rylinaux.plugman.PlugMan;
+import com.rylinaux.plugman.utilities.PluginUtils;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.Plugin;
+
+public class UnloadCommand extends AbstractCommand {
+
+    public static final String DESCRIPTION = "Unload a plugin.";
+
+    public static final String PERMISSION = "plugman.unload";
+
+    public static final String USAGE = "/plugman unload [plugin]";
+
+    public static final String[] SUB_PERMISSIONS = {""};
+
+    public UnloadCommand(CommandSender sender) {
+        super(sender, DESCRIPTION, PERMISSION, SUB_PERMISSIONS, USAGE);
+    }
+
+    @Override
+    public void execute(CommandSender sender, Command command, String label, String[] args) {
+
+        if (!hasPermission()) {
+            sender.sendMessage(PlugMan.getInstance().getMessageManager().format("error.no-permission"));
+            return;
+        }
+
+        if (args.length < 2) {
+            sender.sendMessage(PlugMan.getInstance().getMessageManager().format("error.specify-plugin"));
+            return;
+        }
+
+        Plugin target = PluginUtils.getPluginByName(args, 1);
+
+        if (target == null) {
+            sender.sendMessage(PlugMan.getInstance().getMessageManager().format("error.invalid-plugin"));
+            return;
+        }
+
+        if (target.getName().equalsIgnoreCase("PlugMan")) {
+            sender.sendMessage(PlugMan.getInstance().getMessageManager().format("error.itself"));
+            return;
+        }
+
+        PluginUtils.unload(target);
+
+        sender.sendMessage(PlugMan.getInstance().getMessageManager().format("unload.unloaded", target.getName()));
+
+    }
 }
--- src/main/java/com/rylinaux/plugman/commands/UsageCommand.java
+++ src/main/java/com/rylinaux/plugman/commands/UsageCommand.java
@@ -1,5 +1,49 @@
 package com.rylinaux.plugman.commands;
 
-public class UsageCommand {
+import com.rylinaux.plugman.PlugMan;
+import com.rylinaux.plugman.utilities.PluginUtils;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.Plugin;
+
+public class UsageCommand extends AbstractCommand {
+
+    public static final String DESCRIPTION = "List commands a plugin has registered.";
+
+    public static final String PERMISSION = "plugman.usage";
+
+    public static final String USAGE = "/plugman usage [plugin]";
+
+    public static final String[] SUB_PERMISSIONS = {""};
+
+    public UsageCommand(CommandSender sender) {
+        super(sender, DESCRIPTION, PERMISSION, SUB_PERMISSIONS, USAGE);
+    }
+
+    @Override
+    public void execute(CommandSender sender, Command command, String label, String[] args) {
+
+        if (!hasPermission()) {
+            sender.sendMessage(PlugMan.getInstance().getMessageManager().format("error.no-permission"));
+            return;
+        }
+
+        if (args.length < 2) {
+            sender.sendMessage(PlugMan.getInstance().getMessageManager().format("error.specify-plugin"));
+            return;
+        }
+
+        Plugin target = PluginUtils.getPluginByName(args, 1);
+
+        if (target == null) {
+            sender.sendMessage(PlugMan.getInstance().getMessageManager().format("error.invalid-plugin"));
+            return;
+        }
+
+        String usages = PluginUtils.getUsages(target);
+
+        sender.sendMessage(PlugMan.getInstance().getMessageManager().format("usage.usage", usages));
+
+    }
 }
