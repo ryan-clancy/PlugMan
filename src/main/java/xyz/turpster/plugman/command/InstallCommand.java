3377c65652d5043c2df4213a128b6bda533f3da6
Turpster <turpster@turpster.xyz>
2019-05-22 13:54:06 +0100

    👀 oversights...

--- src/main/java/xyz/turpster/plugman/command/InstallCommand.java
+++ src/main/java/xyz/turpster/plugman/command/InstallCommand.java
@@ -1,4 +1,87 @@
 package xyz.turpster.plugman.command;
 
-public class InstallCommand {
+/*
+ * #%L
+ * PlugMan
+ * %%
+ * Copyright (C) 2010 - 2014 PlugMan
+ * %%
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ * #L%
+ */
+
+import com.rylinaux.plugman.command.AbstractCommand;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+
+/**
+ * Command that installs plugin(s) from Bukkit or Spigot repositories.
+ *
+ * @author turpster
+ */
+public class InstallCommand extends AbstractCommand {
+
+    /**
+     * The name of the command.
+     */
+    public static final String NAME = "install";
+
+    /**
+     * The description of the command.
+     */
+    public static final String DESCRIPTION = "Install a plugin.";
+
+    /**
+     * The main permission of the command.
+     */
+    public static final String PERMISSION = "plugman.install";
+
+    /**
+     * The proper usage of the command.
+     */
+    public static final String USAGE = "/plugman install <plugin>";
+
+    /**
+     * The sub permissions of the command.
+     */
+    public static final String[] SUB_PERMISSIONS = {""};
+
+    /**
+     * Construct out object.
+     *
+     * @param sender the command sender
+     */
+    public InstallCommand(CommandSender sender) {
+        super(sender, NAME, DESCRIPTION, PERMISSION, SUB_PERMISSIONS, USAGE);
+    }
+
+    /**
+     * Execute the command.
+     *
+     * @param sender  the sender of the command
+     * @param command the command being done
+     * @param label   the name of the command
+     * @param args    the arguments supplied
+     */
+    @Override
+    public void execute(CommandSender sender, Command command, String label, String[] args) {
+
+    }
 }
--- src/main/java/xyz/turpster/plugman/command/RemoveCommand.java
+++ src/main/java/xyz/turpster/plugman/command/RemoveCommand.java
@@ -1,18 +1,42 @@
 package xyz.turpster.plugman.command;
 
+/*
+ * #%L
+ * PlugMan
+ * %%
+ * Copyright (C) 2010 - 2014 PlugMan
+ * %%
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ * #L%
+ */
+
 import com.rylinaux.plugman.command.AbstractCommand;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-
-import java.util.List;
 
 /**
  * Command that removes specified plugin(s).
  *
  * @author turpster
  */
-public class RemoveCommand extends AbstractCommand
-{
+public class RemoveCommand extends AbstractCommand {
     /**
      * The name of the command.
      */
@@ -31,7 +55,7 @@ public class RemoveCommand extends AbstractCommand
     /**
      * The proper usage of the command.
      */
-    public static final String USAGE = "/plugman install <plugin-name>";
+    public static final String USAGE = "/plugman remove <plugin>";
 
     /**
      * The sub permissions of the command.
@@ -56,8 +80,7 @@ public class RemoveCommand extends AbstractCommand
      * @param args    the arguments supplied
      */
     @Override
-    public void execute(CommandSender sender, Command command, String label, String[] args)
-    {
+    public void execute(CommandSender sender, Command command, String label, String[] args) {
 
     }
 }
