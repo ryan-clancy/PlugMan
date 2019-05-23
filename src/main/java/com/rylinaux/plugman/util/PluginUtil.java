5202482fe87d7b55ffe452e1e24ccdf7872abc58
Turpster <turpster@turpster.xyz>
2019-05-24 00:10:46 +0100

    jarfile extension may be uppercase

--- src/main/java/com/rylinaux/plugman/util/PluginUtil.java
+++ src/main/java/com/rylinaux/plugman/util/PluginUtil.java
@@ -338,7 +338,7 @@ public class PluginUtil {
 
         if (!pluginFile.isFile()) {
             for (File f : pluginDir.listFiles()) {
-                if (f.getName().endsWith(".jar")) {
+                if (f.getName().toLowerCase().endsWith(".jar")) {
                     try {
                         PluginDescriptionFile desc = PlugMan.getInstance().getPluginLoader().getPluginDescription(f);
                         if (desc.getName().equalsIgnoreCase(name)) {
